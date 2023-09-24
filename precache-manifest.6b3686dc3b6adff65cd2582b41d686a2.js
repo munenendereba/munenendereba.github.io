52d976fa37321bcb97d19d41c19b026e77154ff4
Isaac Munene Ndereba <abeyesark@gmail.com>
2023-09-24 18:56:37 +0300

    Updates

---
+++ precache-manifest.6b3686dc3b6adff65cd2582b41d686a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955287725c35f2ed52a32be0610c401c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b36fe7ab6e9f0136776",
+    "url": "/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "4d0386f83fb3e69ff81f",
+    "url": "/static/css/main.9747798b.chunk.css"
+  },
+  {
+    "revision": "4b36fe7ab6e9f0136776",
+    "url": "/static/js/2.6667b352.chunk.js"
+  },
+  {
+    "revision": "b8a26cc07166dc30cf9055bb104c91cd",
+    "url": "/static/js/2.6667b352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d0386f83fb3e69ff81f",
+    "url": "/static/js/main.89b2e7e9.chunk.js"
+  },
+  {
+    "revision": "a06d9c7e2f0c41efbe9a",
+    "url": "/static/js/runtime-main.6d32849d.js"
+  }
+]);
